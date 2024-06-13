0293f99c44625c22465a4928f8d47b73db25fe5c
Abhishek5723 <104137737+Abhishek5723@users.noreply.github.com>
2024-06-13 17:16:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+
+
+import CustomizedSlider from './CustomizedSlider';
+
+function App() {
+  return (
+    
+     <div className="flex relative items-center justify-center top-[200px] bg-gray-200">
+      
+      
+       <CustomizedSlider/>
+     
+    </div>
+   
+    
+    
+    
+
+  );
+}
+
+export default App;
---
+++ CustomizedSlider.stories.jsx
@@ -0,0 +1,9 @@
+import CustomizedSlider from './CustomizedSlider';
+
+const meta = {
+  component: CustomizedSlider,
+};
+
+export default meta;
+
+export const Default = {};
